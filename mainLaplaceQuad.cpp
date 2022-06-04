fbf36c95166f3bc099f7d1f8930695ce7e603b47
bensinghdhas <bensinghdhas@iisc.ac.in>
2022-06-04 18:42:35 -0500

    made the gauss data as base class and quad data ass a subclass

--- mainLaplaceQuad.cpp
+++ mainLaplaceQuad.cpp
@@ -1,8 +1,7 @@
 #include <eigen-3.4.0/Eigen/Dense> 
 #include <eigen-3.4.0/Eigen/Sparse>
 
-#include "C_FEM_BasisFunction_1D.h"
-#include "C_FEM_GaussPoint_1D.h" 
+#include "C_GaussData.h" 
 #include "C_Matrix_Sparse.h"
 #include "C_Matrix_Dense.h"
 #include "C_Mesh.h"
@@ -13,18 +12,24 @@
 
 int main()
 {
+    C_GaussData2DQuad gpData(3);
     C_Mesh2D mesh;
     mesh.meshRectangle({0,1},{0,1},10,10);
-
-    for (const auto& node: mesh.nodes)
+    for (int i=0; i<gpData.num_GP; i++)
     {
-        std::cout << node[0]<< ';' << node[1]<<'\n';
+        std::cout << gpData.ptx[i]<< ' ' << gpData.pty[i]<<' ' << gpData.wt[i]<<  '\n';
     }
 
-    for (const auto& element: mesh.elements)
-    {
-        std::cout << element[0]<< ';' << element[1]<< ';'<< element[2]<< ';'<< element[3]<<'\n';
-    }
+    // for (const auto& node: mesh.nodes)
+    // {
+    //     std::cout << node[0]<< ';' << node[1]<<'\n';
+    // }
+
+    // for (const auto& element: mesh.elements)
+    // {
+    //     std::cout << element[0]<< ';' << element[1]<< ';'<< element[2]<< ';'<< element[3]<<'\n';
+    // }
+    
 
 
     return 0;
